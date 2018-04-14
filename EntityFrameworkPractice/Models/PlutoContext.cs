59bc14e04e5498fa6597d19d81f00c76266ec126
Damilare Ayodele <damilarea@xceed365.com>
2018-04-14 17:06:59 +0100

    Added DbContext and Connection String

---
+++ EntityFrameworkPractice/Models/PlutoContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+
+namespace EntityFrameworkPractice.Models
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext() : base("name=PlutoDBConnection")
+        {
+            
+        }
+
+    }
+}
